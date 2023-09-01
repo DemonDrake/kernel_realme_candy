67f61a27f922ce49304845ac521130b5b5a1145b
DemonDrake <31922309+DemonDrake@users.noreply.github.com>
2023-09-01 14:06:28 +0800

    Build: Change clang to A12
    also changed the AnyKernel3 source

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 set -e -x
 
 # Final Zip Name
-ZIPNAME="Void-Kernel-test.zip"
+ZIPNAME="Candy-Kernel.zip"
 
 # Kernel Config
 KERNEL_DEFCONFIG="vendor/lahaina-qgki_defconfig"
 
 # Prebuilt Clang Toolchain (AOSP)
-CLANG_URL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android13-release/clang-r450784d.tar.gz"
+CLANG_URL="https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/android12-release/clang-r416183b.tar.gz"
 
 # Prebuilt GCC Utilities (AOSP)
 GCC_x64="https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9"
@@ -46,7 +46,7 @@ SECONDS=0
 make_fun "${KERNEL_DEFCONFIG}"
 make_fun -j"$(nproc --all)" 2>&1 | tee build.log 
 
-git clone --depth=1 https://github.com/cd-Seraph/AnyKernel3.git -b master AnyKernel
+git clone --depth=1 https://github.com/DemonDrake/AnyKernel3.git -b master AnyKernel
 cp out/arch/arm64/boot/Image AnyKernel
 python3 scripts/dtc/libfdt/mkdtboimg.py create AnyKernel/dtbo.img --page_size=4096 out/arch/arm64/boot/dts/vendor/oplus_7325/yupik-21643-overlay.dtbo
 cd AnyKernel
